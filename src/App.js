e3172f3419981c8aff7ebb15fc8816b96b7bdb39
Lcole.engr <lcole.engr@gmail.com>
2020-02-25 23:08:04 -0500

    initializing navbar and pages for portfolio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+
+import Nav from "./Components/Nav";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+
+import{BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import "./styles/custom.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        
+        <Nav />
+        <Switch>
+
+        <Route path = "/" exact component = {Home} />
+        <Route path ="/contact" component = {Contact} />
+        <Route path = "/about" component= {About} />
+      
+        </Switch>
+
+     
+      </Router>
     </div>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+)
 
 export default App;
---
+++ src/Components/Nav.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "../App.css";
+import {Link} from "react-router-dom";
+import "../styles/custom.css";
+
+
+function Nav(){
+
+ const navStyle = {
+   color: "White"
+ };
+
+  return(
+    <nav>
+        <h3>Lavar Cole</h3>
+        <ul className= "nav-links">
+        <Link style ={navStyle} to = "/">
+            <li>Home</li>
+          </Link>
+          <Link style ={navStyle} to = "/about">
+            <li>About</li>
+          </Link>
+          <Link style ={navStyle} to = "/projects">
+            <li>Projects</li>
+          </Link>
+          <Link style ={navStyle} to = "/contact">
+            <li>Contact</li>
+          </Link>
+          
+
+        </ul>
+    </nav>
+  );
+}
+
+
+export default Nav;
