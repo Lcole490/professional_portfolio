315e96c0babc2b0e9e8bbcce4f2e95e9ca48863d
Lcole.engr <lcole.engr@gmail.com>
2020-03-14 13:35:06 -0400

    updated to nav bar component including flex settings, color, and font

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -12,7 +12,9 @@ function Nav(){
 
   return(
     <nav>
+      <div class= "logo">
         <h3>Lavar Cole</h3>
+        </div>
         <ul className= "nav-links">
         <Link style ={navStyle} to = "/">
             <li>Home</li>
@@ -29,6 +31,11 @@ function Nav(){
           
 
         </ul>
+        <div class= "burger">
+          <div class= "line1"></div>
+          <div class= "line2"></div>
+          <div class= "line3"></div>
+        </div>
     </nav>
   );
 }
